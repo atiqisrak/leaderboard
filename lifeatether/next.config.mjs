0c4868e4499563eff6a787cd495e859212f38c61
atiqisrak <atiq.israk@webable.digital>
2025-05-28 20:50:02 +0600

    updated nextconfig

--- lifeatether/next.config.mjs
+++ lifeatether/next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["randomuser.me", "localhost", "127.0.0.1", "vercel.app"],
+    domains: [
+      "randomuser.me",
+      "localhost",
+      "127.0.0.1",
+      "vercel.app",
+      "188.166.232.67",
+    ],
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
