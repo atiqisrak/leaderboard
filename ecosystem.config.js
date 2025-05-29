36552bea636a4a09a520029774ec769754722c6b
Atiq Israk <atiqisrak@gmail.com>
2025-05-29 11:10:42 +0600

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,5 +17,22 @@ module.exports = {
       out_file: "/var/www/leaderboard/lifeatether-engine/logs/output.log",
       time: true,
     },
+    {
+      name: "lifeatether",
+      script: "npm",
+      args: "start",
+      cwd: "/var/www/leaderboard/lifeatether",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3099,
+      },
+      error_file: "/var/www/leaderboard/lifeatether/logs/error.log",
+      out_file: "/var/www/leaderboard/lifeatether/logs/output.log",
+      time: true,
+    },
   ],
 };
